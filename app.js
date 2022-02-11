2f8f0a70f6f31690a988ed066c117653897a3149
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-11 19:34:39 +0100

    change color to all <rect>

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-console.log("it is me Rokinos");
 const columns = Array.from(
   document.querySelectorAll(".js-calendar-graph > svg > g > g")
-);
+).map((group) => Array.from(group.querySelectorAll("rect")));
 
-console.log(columns);
+const emptyColor = "#161b22";
+const fillColor = "#39d353";
+
+// columns[1][0].style.fill = fillColor;
+columns.forEach((c) => c.forEach((f) => (f.style.fill = fillColor)));
